c69aad68990420691f51d734b9369241c617b19e
KurodaKento0505 <kuroda.kento@image.iit.tsukuba.ac.jp>
2024-09-17 21:17:05 +0900

    change

--- tools/inference_sam2.py
+++ tools/inference_sam2.py
@@ -19,6 +19,7 @@ from sklearn.metrics import jaccard_score
 
 from evaluate_sam2 import show_prompt
 from fine_tuning_sam2 import read_batch
+from fine_tuning_sam2 import parse_arguments
 
 def read_image(image_path, mask_path):  # read and resize image and mask
     # Get full paths
@@ -49,14 +50,6 @@ def read_image(image_path, mask_path):  # read and resize image and mask
             points.append([yx[1], yx[0]])
 
     return Img, binary_mask, points, len(inds)
-
-def get_points(mask, num_points):  # Sample points inside the input mask
-    points = []
-    coords = np.argwhere(mask > 128)
-    for i in range(num_points):
-        yx = np.array(coords[np.random.randint(len(coords))])
-        points.append([yx[1], yx[0]])
-    return np.array(points)
 
 # IoUを計算する関数
 def compute_iou(pred_mask, true_mask):
@@ -109,8 +102,80 @@ def show_predicted_image(predicted_masks, scores, image):
     plt.tight_layout()
     plt.show()
 
-if __name__ == '__main__':
+def inference(num_image, num_train_step, image, mask, input_point, num_masks):
 
+    # Load the model
+    checkpoint = "sam2_hiera_base_plus.pt"
+    model_cfg = "sam2_hiera_b+.yaml"
+    sam2_model = build_sam2(model_cfg, checkpoint, device="cuda")
+
+    # Build net and load weights
+    predictor = SAM2ImagePredictor(sam2_model)
+
+    # Load the fine-tuned model
+    model_path = "fine_tuned_model//" + str(num_image) + "//fine_tuned_sam2_"+ str(num_train_step) + ".torch"
+    predictor.model.load_state_dict(torch.load(model_path))
+
+    # 全テストデータに対するIoUを計算
+    total_iou = 0
+    count = 0
+
+    NO_OF_STEPS = 1
+
+    for step in range(1, NO_OF_STEPS + 1):
+        with torch.cuda.amp.autocast():
+            if image is None or mask is None or num_masks == 0:
+                continue
+
+            input_label = np.ones((num_masks, 1))
+            if not isinstance(input_point, np.ndarray) or not isinstance(input_label, np.ndarray):
+                continue
+
+            if input_point.size == 0 or input_label.size == 0:
+                continue
+
+            predictor.set_image(image)
+            mask_input, unnorm_coords, labels, unnorm_box = predictor._prep_prompts(input_point, input_label, box=None, mask_logits=None, normalize_coords=True)
+            if unnorm_coords is None or labels is None or unnorm_coords.shape[0] == 0 or labels.shape[0] == 0:
+                continue
+
+            # プロンプト埋め込みの生成
+            sparse_embeddings, dense_embeddings = predictor.model.sam_prompt_encoder(
+                points=(unnorm_coords, labels), boxes=None, masks=None,
+            )
+
+            # 特徴量とマスクの予測
+            batched_mode = unnorm_coords.shape[0] > 1
+            high_res_features = [feat_level[-1].unsqueeze(0) for feat_level in predictor._features["high_res_feats"]]
+            low_res_masks, prd_scores, _, _ = predictor.model.sam_mask_decoder(
+                image_embeddings=predictor._features["image_embed"][-1].unsqueeze(0),
+                image_pe=predictor.model.sam_prompt_encoder.get_dense_pe(),
+                sparse_prompt_embeddings=sparse_embeddings,
+                dense_prompt_embeddings=dense_embeddings,
+                multimask_output=True,
+                repeat_image=batched_mode,
+                high_res_features=high_res_features,
+            )
+            # 予測マスクの後処理
+            prd_masks = predictor._transforms.postprocess_masks(low_res_masks, predictor._orig_hw[-1])
+            print('prd_masks:', prd_masks.shape)
+            # 正解マスクと予測マスクの準備
+            gt_mask = torch.tensor(mask.astype(np.float32)).cuda()
+            prd_mask = torch.sigmoid(prd_masks[:, 0])
+
+            inter = (gt_mask * (prd_mask > 0.5)).sum(1).sum(1)
+            union = gt_mask.sum(1).sum(1) + (prd_mask > 0.5).sum(1).sum(1) - inter
+            iou = inter / union
+            current_mean_iou = np.mean(iou.cpu().detach().numpy())
+            print('inter:', inter)
+            print('union:', union)
+            print('current_mean_iou:', current_mean_iou)
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    num_train_step = args.num_train_step
+    num_images = [int(num_image) for num_image in args.num_image.split(",")]
+    
     # train_data に image と mask を保存
     test_data = []
 
@@ -127,57 +192,7 @@ if __name__ == '__main__':
             "image": image_file_path,
             "annotation": mask_file_path
         })
-
-    # Load the model
-    checkpoint = "sam2_hiera_base_plus.pt"
-    model_cfg = "sam2_hiera_b+.yaml"
-    sam2_model = build_sam2(model_cfg, checkpoint, device="cuda")
-
-    # Build net and load weights
-    predictor = SAM2ImagePredictor(sam2_model)
-
-    # Load the fine-tuned model
-    # FINE_TUNED_MODEL_WEIGHTS = "fine_tuned_sam2_3000.torch"
-    # predictor.model.load_state_dict(torch.load(FINE_TUNED_MODEL_WEIGHTS))
-
-    # 全テストデータに対するIoUを計算
-    total_iou = 0
-    count = 0
-
-    for data_entry in test_data:
-        image_path = data_entry['image']
-        mask_path = data_entry['annotation']
-
-        # Load image, mask and point
-        image, annotated_mask, input_points, num_masks = read_image(image_path, mask_path)
-        input_points = np.array(input_points)
-        # input_label = np.array([1])
-        # show_prompt(image, input_points, input_label)
-
-        # Skip if no points were found
-        if input_points.size == 0:
-            continue
-
-        # Perform inference and predict masks
-        with torch.no_grad():
-            predictor.set_image(image)
-            predicted_masks, scores, logits = predictor.predict(
-                point_coords=input_points,
-                point_labels=np.ones([input_points.shape[0]])
-            )
-        
-        # 最もIoUのスコアが高かったマスクを記録
-        best_IoU = -1
-        for k in range(len(predicted_masks)):
-            iou_score = compute_iou(annotated_mask, predicted_masks[k])
-            if iou_score > best_IoU:
-                best_IoU = iou_score
-                best_mask = predicted_masks[k]
-
-        total_iou += best_IoU
-        count += 1
-
-        print(count, best_IoU, image_path, mask_path)
-    
-    mean_iou = total_iou / count
-    print(f"Mean IoU across all test images: {mean_iou:.4f}")
+    image, mask, input_point, num_masks = read_batch(test_data, visualize_data=False)
+    for num_image in num_images:
+        print("\n", num_image)
+        inference(num_image, num_train_step, image, mask, input_point, num_masks)
